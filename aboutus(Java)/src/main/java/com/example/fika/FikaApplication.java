6b52125302a524b5844c879c504c7bba2780bf52
G-Mahdi <mguel050@uottawa.ca>
2024-10-27 22:38:06 -0400

    Add files via upload

---
+++ aboutus(Java)/src/main/java/com/example/fika/FikaApplication.java
@@ -0,0 +1,13 @@
+package com.example.fika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FikaApplication.class, args);
+	}
+
+}
---
+++ aboutus(Java)/src/test/java/com/example/fika/FikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
